886b0d3e202b9a440112c60984ff8a77ca6a247f
aleksandrkostin7890 <aleksandrkostin7890@gmail.com>
2024-04-18 21:50:03 +0300

    Add project files.

---
+++ WindowsFormsApp6/Computer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public class Computer
+    {
+        // Поля класу
+        private string type;
+        private string processor;
+        private string monitor;
+        private string keyboard;
+        private string mouse;
+        private string systemUnit;
+        // Конструктор
+        public Computer(string type, string processor, string monitor, string keyboard, string mouse, string systemUnit)
+        {
+            this.type = type;
+            this.processor = processor;
+            this.monitor = monitor;
+            this.keyboard = keyboard;
+            this.mouse = mouse;
+            this.systemUnit = systemUnit;
+        }
+        // Методи класу
+        public void Modernize(string component, string replacement)
+        {
+            switch (component.ToLower())
+            {
+                case "processor":
+                    processor = replacement;
+                    break;
+                case "monitor":
+                    monitor = replacement;
+                    break;
+                case "keyboard":
+                    keyboard = replacement;
+                    break;
+                case "mouse":
+                    mouse = replacement;
+                    break;
+                case "system unit":
+                    systemUnit = replacement;
+                    break;
+                default:
+                    MessageBox.Show("Invalid component name.");
+                    break;
+            }
+        }
+        // Перевірка готовності комп'ютера
+        public bool IsReady()
+        {
+            return !string.IsNullOrWhiteSpace(type) &&
+                   !string.IsNullOrWhiteSpace(processor) &&
+                   !string.IsNullOrWhiteSpace(monitor) &&
+                   !string.IsNullOrWhiteSpace(keyboard) &&
+                   !string.IsNullOrWhiteSpace(mouse) &&
+                   !string.IsNullOrWhiteSpace(systemUnit);
+        }
+        // Отримання опису комп'ютера
+        public string GetDescription()
+        {
+            return $"Type: {type}, Processor: {processor}, Monitor: {monitor}, Keyboard: {keyboard}, Mouse: {mouse}, System Unit: {systemUnit}, Ready: {(IsReady() ? "Yes" : "No")}";
+        }
+    }
+}
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {  
+        List<Computer> computers = new List<Computer>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ClearTextBoxes()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+        }
+        // Додати комп'ютер
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string type = textBox1.Text;
+            string processor = textBox2.Text;
+            string monitor = textBox3.Text;
+            string keyboard = textBox4.Text;
+            string mouse = textBox5.Text;
+            string systemUnit = textBox6.Text;
+
+            Computer computer = new Computer(type, processor, monitor, keyboard, mouse, systemUnit);
+            computers.Add(computer);
+
+            ClearTextBoxes();
+        }
+        // Показати список комп'ютерів
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (var computer in computers)
+            {
+                listBox1.Items.Add(computer.GetDescription());
+            }
+        }
+        // Модернізувати комп'ютер
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = listBox1.SelectedIndex;
+            if (selectedIndex >= 0 && selectedIndex < computers.Count)
+            {
+                Computer computer = computers[selectedIndex];
+
+                string component = textBox7.Text;
+                string replacement = textBox8.Text;
+
+                computer.Modernize(component, replacement);
+
+                listBox1.Items[selectedIndex] = computer.GetDescription();
+            }
+            else
+            {
+                MessageBox.Show("Please select a computer to modernize.");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
